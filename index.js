5884ae6021fc11a3673075442f81fd4e0e1e1acb
Matt Basta <mattbasta@gmail.com>
2016-03-22 21:29:58 -0700

    Additions because we're bad at math

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 module.exports = function(robot) {
-    robot.respond(/roll (\w+)$/, function(msg, done) {
+    robot.respond(/roll ([\w\+ ]+)$/, function(msg, done) {
         var die = msg.match[1];
 
-        var breakdown = /(\d+)?d(\d+)/.exec(die);
+        var breakdown = /(\d+)?d(\d+) *(\+\s*\d+)?/.exec(die);
         if (!breakdown) {
             done();
             return;
         }
         var sides = breakdown[2] | 0;
         var count = 1;
+        var add = 0;
         if (breakdown[1]) {
             count = breakdown[1] | 0;
+        }
+        if (breakdown[3]) {
+            add = parseInt(breakdown[3].substr(1), 10);
         }
 
         var rolls = [];
         for (var i = 0; i < count; i++) {
-            rolls.push(roll(sides));
+            rolls.push(roll(sides, add));
         }
 
         msg.reply(rolls.join('\n'), done);
@@ -23,9 +27,13 @@ module.exports = function(robot) {
 };
 
 
-function roll(sides) {
+function roll(sides, add) {
     var r = (Math.random() * sides | 0) + 1;
+    var total = r + add;
     var output = 'Rolled ' + r.toString();
+    if (add) {
+        output += ' + ' + add.toString() + ' = ' + total.toString();
+    }
     if (sides === 20) {
         if (r === 20) {
             output += ' (NAT 20!)';
